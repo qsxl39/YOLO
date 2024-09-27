0c7dadc7710daf80c0b046660fd9fa0afad6e033
qsxl39 <2045872352@qq.com>
2024-09-27 21:15:51 +0800

    manage

--- src/views/alarms/manage/manage.vue
+++ src/views/alarms/manage/manage.vue
@@ -20,7 +20,7 @@
     </el-table-column>
     <el-table-column prop="peizhi" label="算法配置" style="width: 10%" align="center">
       <template #default="">
-        <el-icon color="#409efc" class="no-inherit" :size="20" @click="handleSettingClick(scope.$index, scope.row)">
+        <el-icon color="#409efc" class="no-inherit" :size="20" @click=" dialogTableVisible = true">
           <setting />
         </el-icon>
       </template>
@@ -31,17 +31,67 @@
       <template #default="scope">{{ scope.row.shijian }}</template>
     </el-table-column>
   </el-table>
+  <div class="dialog-footer" style="margin: 5px;">
+    <el-button >重启通道</el-button>
+    <el-button >重置配置</el-button>
+  </div>
   <!-- 对话框 -->
-  <myDialog/>
+  <el-dialog v-model="dialogTableVisible" title="算法配置" width="1100">
+    <el-table border stripe :data="gridData" style="width: 100%">
+      <el-table-column property="mingcheng" label="算法名称" width="120" align="center" />
+      <el-table-column label="开关状态" width="120" align="center">
+        <template #default="{ row }">
+          <el-switch v-model="row.zhuangtai" size="large" inline-prompt active-text="开" inactive-text="关" />
+        </template>
+      </el-table-column>
+      <el-table-column label="灵敏度" width="600" align="center">
+        <template #default="{ row }">
+          <div class="slider-demo-block">
+            <el-slider v-model="row.lingmindu" show-input :min="0" :max="100" />
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column label="上报频率" align="center">
+        <template #default="{ row }">
+          <el-input-number v-model="row.pinlv" :min="1" :max="10" />
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="dialog-footer" style="margin: 5px;">
+      <el-button type="primary" @click="saveChanges">保存</el-button>
+      <el-button @click="dialogTableVisible = false">取消</el-button>
+    </div>
+  </el-dialog>
 
 </template>
 
 <script lang="ts" setup>
 import { ElTable, ElInput } from 'element-plus'
 import { Setting } from '@element-plus/icons-vue'
-import myDialog from './myDialog.vue';
+import { ref } from 'vue';
 
-//绑定事件
+const dialogTableVisible = ref(false);
+
+const gridData = ref([
+  {
+    mingcheng: '人数统计',
+    zhuangtai: true,
+    lingmindu: 50, // 假设灵敏度初始值为50  
+    pinlv: 1,      // 假设上报频率初始值为1  
+  },
+  {
+    mingcheng: '车辆统计',
+    zhuangtai: false,
+    lingmindu: 30, // 假设灵敏度初始值为30  
+    pinlv: 5,      // 假设上报频率初始值为5  
+  },
+]);
+
+const saveChanges = () => {
+  dialogTableVisible.value = false
+  console.log('保存的数据:', gridData.value);
+  // 这里可以添加将gridData发送到服务器的逻辑  
+};  
 
 
 
@@ -60,23 +110,55 @@ const tableData: User[] = [
   {
     number: '1',
     zhuangtai: true,
-    dizhi: 'aaaaa',
-    mingcheng: 'a',
-    peizhi: 'a',
+    dizhi: 'rtsp://xxxx',
+    mingcheng: 'ID-00',
+    peizhi: 'ID-00',
     zhouije: '4',
-    shijian: '2016-05-04'
+    shijian: '2016-05-04 23:12:11'
   },
   {
-    number: '1',
+    number: '2',
     zhuangtai: true,
-    dizhi: 'aaaaa',
-    mingcheng: 'a',
-    peizhi: 'a',
+    dizhi: 'rtsp://xxxx',
+    mingcheng: 'ID-01',
+    peizhi: 'ID-00',
     zhouije: '4',
-    shijian: '2016-05-04'
+    shijian: '2016-05-04 23:12:11'
+  },
+  {
+    number: '3',
+    zhuangtai: true,
+    dizhi: 'rtsp://xxxx',
+    mingcheng: 'ID-02',
+    peizhi: 'ID-00',
+    zhouije: '4',
+    shijian: '2016-05-04 23:12:11'
+  },
+  {
+    number: '4',
+    zhuangtai: false,
+    dizhi: 'rtsp://xxxx',
+    mingcheng: 'ID-03',
+    peizhi: 'ID-00',
+    zhouije: '4',
+    shijian: '2016-05-04 23:12:11'
   }
 ]
 
 // 对话框
 
 </script>
+
+
+<style scoped>
+.slider-demo-block {
+  max-width: 600px;
+  display: flex;
+  align-items: center;
+}
+
+.slider-demo-block .el-slider {
+  margin-top: 0;
+  margin-left: 12px;
+}
+</style>
